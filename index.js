806c59e51162bee00d46a9ffbb8fe9d3fb293f09
isauljosue <56583387+isauljosue@users.noreply.github.com>
2021-05-27 15:28:35 -0500

    part 3

---
+++ index.js
@@ -0,0 +1,116 @@
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Person = require('./models/person')
+
+const cors = require('cors')
+const morgan = require('morgan')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+morgan.token('content', (request) => {
+  return request.method === 'POST'
+    ? JSON.stringify(request.body)
+    : null
+})
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('-----------')
+  next()
+}
+
+app.use(requestLogger)
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+})
+
+app.get('/info', (request, response) => {
+  const personsAmount = Person.length
+  const date = new Date()
+  response.send(`<h1>Phonebook currently has ${personsAmount} contacts</h1><div>${date}<div/>`)
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ mongo.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://isaul:${password}@cluster0.jpkhh.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => console.log(person.name, person.number))
+    mongoose.connection.close()
+  })
+
+} else {
+  const newPerson = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  console.log(newPerson)
+  console.log('---------')
+
+  newPerson.save().then(() => {
+    console.log(`added ${newPerson.name} with number ${newPerson.number} to the phonebook`)
+    mongoose.connection.close()
+  })
+}
